942439e4d29aa3f0d883948ed95da86dc2c06510
BaBaSaMa <64760317+Ju-Long@users.noreply.github.com>
2022-06-28 14:03:48 +0800

    Revert "update"
    This reverts commit fc68ca267da1fa15cec34f7dbdf5610563117323.

--- Lesson 14/dbFunctions.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$HOST = "localhost";
-$USERNAME = "root";
-$PASSWORD = "";
-$DB = "c302_p13";
-$link = mysqli_connect($HOST,$USERNAME,$PASSWORD,$DB) or 
-        die(mysqli_connect_error());
